18e5b435e018debdf10bd496cb87446dbbe75ef4
yarocruz <yarocruz@gmail.com>
2021-10-17 13:31:58 -0400

    added basic transpiling from editor to preview code div

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
-import {useState} from "react";
+import * as esbuild from 'esbuild-wasm'
+import {useState, useEffect, useRef} from "react";
 import ReactDOM from 'react-dom';
 
-
 const App = () => {
+    const ref = useRef<any>()
+
     const [input, setInput] = useState('')
     const [code, setCode] = useState('')
 
-    const onClick = () => {
-        console.log(input)
+    // we are grabbing the startService method from esbuild
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+    useEffect(() => {
+        // starting the esbuild service when the app loads for the first time
+        startService()
+    }, []);
+
+
+    const onClick = async () => {
+       if (!ref.current) {
+           return;
+       }
+       // using the transform from our ref to take the string from input and transpile
+       const result = await ref.current.transform(input, {
+           loader: 'jsx',
+           target: 'es2015'
+       })
+
+       setCode(result.code)
     }
     return (
         <div>
