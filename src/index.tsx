21970fc7cf6c58894d9883b88abf8e4a80f49ae5
yarocruz <yarocruz@gmail.com>
2021-10-17 13:03:01 -0400

    setup a very basic prototype of the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
+import {useState} from "react";
 import ReactDOM from 'react-dom';
 
+
 const App = () => {
-    return <div>hi</div>
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const onClick = () => {
+        console.log(input)
+    }
+    return (
+        <div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    )
 }
 
 ReactDOM.render(
