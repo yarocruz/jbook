62a56f45b1f0363e5d289ff7b84ff84346e18720
yarocruz <yarocruz@gmail.com>
2021-10-21 19:43:42 -0400

    added checks for module resution path issues when have nested files / aka dependencies

--- src/unpkg-path-plugin.ts
+++ src/unpkg-path-plugin.ts
@@ -1,4 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from "axios";
+
+/* This Plugin is hijacking the default way the esbuild bundles files */
 
 export const unpkgPathPlugin = () => {
     return {
@@ -6,9 +9,26 @@ export const unpkgPathPlugin = () => {
         setup(build: esbuild.PluginBuild) {
             build.onResolve({ filter: /.*/}, async (args: any) => {
                 console.log('onResolve', args)
-                return { path: args.path, namespace: 'a'}
+                if (args.path === 'index.js') {
+                    return { path: args.path, namespace: 'a'}
+                }
+                // in the case when we get nested files inside the main file from the args.path
+                if (args.path.includes('./') || args.path.includes('../')) {
+                    return {
+                        namespace: 'a',
+                        // construct the path with the directory
+                        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+                    }
+                }
+                // the case for just the a plain index.js file
+                return {
+                    namespace: 'a',
+                    path: `https://unpkg.com/${args.path}`
+                }
+
             })
 
+            // @ts-ignore
             build.onLoad({ filter: /.*/}, async (args: any) => {
                 console.log('onLoad', args)
 
@@ -16,15 +36,18 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-                            import message from './message';
+                            const message = require('nested-test-pkg');
                             console.log(message);
                         `
                     }
-                } else {
-                    return {
-                        loader: 'jsx',
-                        contents: 'export default "hi there!"',
-                    }
+                }
+
+                const { data, request } = await axios.get(args.path);
+
+                return {
+                    loader: 'jsx',
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname
                 }
             })
         }
