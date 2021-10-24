52435d2ba8c9a2003b3ead3854981853cad2c193
yarocruz <yarocruz@gmail.com>
2021-10-24 13:27:26 -0400

    defined the NODE_ENV for production

--- src/index.tsx
+++ src/index.tsx
@@ -37,7 +37,11 @@ const App = () => {
            entryPoints: ['index.js'],
            bundle: true,
            write: false,
-           plugins: [unpkgPathPlugin()]
+           plugins: [unpkgPathPlugin()],
+           define: {
+               'process.env.NODE_ENV': '"production"',
+               global: 'window'
+           }
        })
 
 
