3a385b927e981f56b569e65bdd9944ef65ded376
yarocruz <yarocruz@gmail.com>
2021-10-24 13:25:26 -0400

    brought in localforage to cache files and avoid unnecessary requests

--- src/unpkg-path-plugin.ts
+++ src/unpkg-path-plugin.ts
@@ -1,12 +1,26 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from "axios";
+import localForage from "localforage";
 
-/* This Plugin is hijacking the default way the esbuild bundles files */
+const fileCache = localForage.createInstance({
+    name: 'filecache'
+});
+
+// (async () => {
+//    await fileCache.setItem('color', 'red')
+//
+//    const color = await fileCache.getItem('color')
+//
+//     console.log(color)
+// })()
+
+/* This Plugin is hijacking the default way esbuild bundles files */
 
 export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
+
             build.onResolve({ filter: /.*/}, async (args: any) => {
                 console.log('onResolve', args)
                 if (args.path === 'index.js') {
@@ -20,7 +34,7 @@ export const unpkgPathPlugin = () => {
                         path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
                     }
                 }
-                // the case for just the a plain index.js file
+                // the case for just a plain index.js file
                 return {
                     namespace: 'a',
                     path: `https://unpkg.com/${args.path}`
@@ -28,7 +42,6 @@ export const unpkgPathPlugin = () => {
 
             })
 
-            // @ts-ignore
             build.onLoad({ filter: /.*/}, async (args: any) => {
                 console.log('onLoad', args)
 
@@ -36,19 +49,32 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-                            const message = require('nested-test-pkg');
-                            console.log(message);
+                            import React, { useState } from 'react';
+                            import ReactDOM from 'react-dom';
+                            console.log(react, reactDOM);
                         `
                     }
                 }
 
-                const { data, request } = await axios.get(args.path);
+                // Checking to see if we already fetched a file
+                // return immediately if true
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
 
-                return {
+                if (cachedResult) {
+                    return cachedResult
+                }
+
+                const { data, request } = await axios.get<string | Uint8Array | undefined>(args.path);
+
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
-                }
+                };
+                // store response in cache
+                await fileCache.setItem(args.path, result)
+
+                return result
             })
         }
     }
